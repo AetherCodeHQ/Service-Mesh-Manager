64a734b28b2c0e23dba514d394137a12efba2d30
AetherCodeHQ <noreply@users.noreply.github.com>
2026-08-24 15:58:35 +0300

    v2.1.0: unique implementation replaces generic stub

--- main.go
+++ main.go
@@ -1,37 +1,64 @@
-
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"net"
 	"os"
-	"strconv"
-	"strings"
-	"time"
+	"sort"
 )
 
+type service struct {
+	Name string
+	Host string
+	Port int
+	Tags []string
+}
+
 func main() {
-	host := "127.0.0.1"
-	ports := []int{22, 80, 443, 8080}
-	if len(os.Args) > 1 {
-		host = os.Args[1]
+	if len(os.Args) < 2 {
+		fmt.Println("usage: service-mesh-manager <registry.json> [list|register|route]")
+		os.Exit(1)
 	}
+	registryPath := os.Args[1]
+	data, _ := os.ReadFile(registryPath)
+	var services []service
+	if len(data) > 0 {
+		json.Unmarshal(data, &services)
+	}
+	cmd := "list"
 	if len(os.Args) > 2 {
-		ports = ports[:0]
-		for _, q := range strings.Split(os.Args[2], ",") {
-			if n, err := strconv.Atoi(q); err == nil {
-				ports = append(ports, n)
+		cmd = os.Args[2]
+	}
+	switch cmd {
+	case "list":
+		for _, s := range services {
+			fmt.Printf("%-20s %s:%d %v\n", s.Name, s.Host, s.Port, s.Tags)
+		}
+		fmt.Printf("\n%d services registered\n", len(services))
+	case "register":
+		if len(os.Args) < 5 {
+			fmt.Fprintln(os.Stderr, "need name host:port")
+			os.Exit(1)
+		}
+		s := service{Name: os.Args[3], Host: os.Args[4]}
+		services = append(services, s)
+		b, _ := json.Marshal(services)
+		os.WriteFile(registryPath, b, 0644)
+		fmt.Println("registered", s.Name)
+	case "route":
+		if len(os.Args) < 4 {
+			fmt.Fprintln(os.Stderr, "need service name")
+			os.Exit(1)
+		}
+		name := os.Args[3]
+		for _, s := range services {
+			if s.Name == name {
+				fmt.Printf("route: %s -> %s:%d\n", name, s.Host, s.Port)
+				return
 			}
 		}
+		fmt.Println("service not found")
+		os.Exit(1)
 	}
-	for _, p := range ports {
-		addr := fmt.Sprintf("%s:%d", host, p)
-		c, err := net.DialTimeout("tcp", addr, 2*time.Second)
-		if err != nil {
-			fmt.Printf("%-24s closed\n", addr)
-			continue
-		}
-		fmt.Printf("%-24s open\n", addr)
-		c.Close()
-	}
+	_ = sort.IntSlice{}
 }
